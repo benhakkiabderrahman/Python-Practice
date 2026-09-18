ae32d07ed5dbe8f8cd5a7266790ed490acfda874
Benhakki Abderrahman <benhakkiabderrahman@gmail.com>
2026-09-18 15:11:53 +0100

    Initial commit: Python practice files

---
+++ First boss fight/Number & Password Analyzer.py
@@ -0,0 +1,36 @@
+password = "56751746a" 
+attempt = 3
+
+while attempt > 0: 
+    attempt -= 1
+    entred_password = input("Please enter the password: ")
+    password_length = len(entred_password)
+    digit_only = entred_password.isdigit()
+    
+    if entred_password == password:
+        if password_length >= 8 and digit_only == False:
+            print("Correct Password!")
+            
+            # --- بداية مهمة محلل الأرقام بعد النجاح ---
+            print("\nNow let's see if the number is Positive/Negative/Zero and Even/Odd.")
+            number = int(input("Please enter the number: "))
+            even = number % 2
+            
+            if number > 0 and even == 0:
+                print("The number is Positive and Even")
+            elif number > 0 and even != 0:
+                print("The number is Positive and Odd")
+            elif number < 0 and even == 0:
+                print("The number is Negative and Even")
+            elif number < 0 and even != 0:
+                print("The number is Negative and Odd")
+            elif number == 0 and even == 0:
+                print("The number is Zero and Even")
+            # --- نهاية مهمة محلل الأرقام ---
+            
+            break # نكسر حلقة كلمة المرور وننهي البرنامج
+            
+        else:
+            print("Verify the lengh of password typed , or is it only digits.")
+    else:
+        print("Wrong, Type the password agin please.")
---
+++ hello.py
@@ -0,0 +1,7 @@
+numbers = [1, 62, -3, 4, 5]
+
+largest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        largest = num 
